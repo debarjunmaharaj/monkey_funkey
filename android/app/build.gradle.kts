db7e509cbc387e036278d869be6987f5b2228091
Debarjun Chakraborty <Debarjunofficial@gmail.com>
2026-06-15 21:18:39 +0600

    toss

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.netfie.monkey_funkey.monkey_funkey"
+    namespace = "com.netfie.monkeyfunkey.game"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -16,20 +16,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.netfie.monkey_funkey.monkey_funkey"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.netfie.monkeyfunkey.game"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 4
+        versionName = "1.4"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- android/app/src/main/kotlin/com/netfie/monkey_funkey/monkey_funkey/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.netfie.monkey_funkey.monkey_funkey
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
