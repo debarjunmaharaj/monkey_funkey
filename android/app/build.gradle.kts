bfc6cc83da22025213ed5eef7910c2050b871bbb
Debarjun Chakraborty <Debarjunofficial@gmail.com>
2026-06-17 16:24:45 +0600

    toss

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.netfie.monkeyfunkey.game"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.5"
     }
 
     buildTypes {
